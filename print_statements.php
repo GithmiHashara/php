65ad0d7695819d060d9581e215b8bfd2a746b7f1
Githmi Hashara <pdghashara@std.appsc.sab.ac.lk>
2024-10-03 22:48:36 +0530

    Add PHP files to display a statement

---
+++ print_statements.php
@@ -0,0 +1,7 @@
+<?php
+
+//Write down a PHP program to display “The pen is “mightier”” than the sword” in your web browser. 
+
+echo "The pen is \"mightier\" than the sword";
+
+
---
+++ web_statement.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>PHP Display Example</title>
+    </head>
+    <body>
+        <?php
+            echo 'The pen is "mightier" than the sword';
+        ?>
+    </body>
+</html>
