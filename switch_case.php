a039440aaa6f4cbee187cc165d8769a7094fe268
Githmi Hashara <pdghashara@std.appsc.sab.ac.lk>
2024-10-03 21:40:10 +0530

    Add switch case statement to display user's favorite color

---
+++ switch_case.php
@@ -0,0 +1,16 @@
+<?php
+$favcolour = "Red";
+
+switch ($favcolour) {
+    case "Red":
+        echo "Your favourite colour is Red!";
+        break;
+    case "Blue":
+        echo "Your favourite colour is Blue!";
+        break;
+    case "Green":
+        echo "Your favourite colour is Green!";
+        break;
+    default:
+        echo "Your favourite colour is neither Red, Blue, nor Green!";
+}
