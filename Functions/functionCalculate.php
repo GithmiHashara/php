ef19ba8b50630a3334120fe1681fa5f6d1efdb2d
Githmi Hashara <pdghashara@std.appsc.sab.ac.lk>
2024-10-04 00:26:51 +0530

    Add functions

---
+++ Functions/functionCalculate.php
@@ -0,0 +1,15 @@
+<?php
+
+$x = 10;
+
+//Function to calculate f(x)
+function calculate($x){
+    return 5*$x + 10;
+}
+
+//calculate function
+$f = calculate($x);
+
+//Display result
+echo " For x = $x, f(x) = $f \n";
+
---
+++ Functions/rootFunctions.php
@@ -0,0 +1,10 @@
+<?php
+
+$x = 16;
+function calculate ($x){
+    return (sqrt($x) + 20);
+}
+
+$f = calculate($x);
+echo "For x =$x , f(x) = $f \n";
+
