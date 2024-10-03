70527cb419edb6ca17c4faf15048b6353dc652d4
Githmi Hashara <pdghashara@std.appsc.sab.ac.lk>
2024-10-03 18:58:00 +0530

    Refactor Function2.php to include additional functions for calculating f(x,y) and f(x,y,z)

--- Function2.php
+++ Function2.php
@@ -3,6 +3,7 @@
 //variable value assign
 $x= 10;
 $y= 5;
+$z = 20;
 
 //function declaration
 function calculate($x){
@@ -23,3 +24,19 @@ function calculate2($y){
 
 $f2 = calculate2($y);
 echo "For y = $y, f(y) = $f2 \n";
+
+//function3
+function calculate3($x, $y){
+    return $x + $y;
+}
+
+$f3 = calculate3($x, $y);
+echo "For x = $x and y = $y, f(x,y) = $f3 \n";
+
+//function4
+function calculate4($x, $y, $z) {
+    return 2* $x + $y + ($z/2);
+}
+
+$f4 = calculate4($x, $y, $z);
+echo " For x = $x, y = $y and z = $z, f(x,y,z) = $f4 \n";
