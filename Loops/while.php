0ade0e023f343feef3c6362c8782641a4cf26b5b
Githmi Hashara <pdghashara@std.appsc.sab.ac.lk>
2024-10-04 02:04:31 +0530

    Add while loop to iterate and display numbers with different conditions

--- Loops/while.php
+++ Loops/while.php
@@ -16,3 +16,14 @@ while ($i < 6){
 
 //The while loop syntax can also be written with the endwhile statement like this
 
+while ($i < 3):
+ echo $i . "\n";
+ $i++;
+ endwhile;
+
+//count until 100 but increase by 10
+
+while ($i <= 100):
+    echo $i . "\n";
+    $i += 10;
+endwhile;
