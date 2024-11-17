184ac63406699f2490f9a7fcd06f84185a204350
Githmi Hashara <pdghashara@std.appsc.sab.ac.lk>
2024-11-17 21:57:38 +0530

    Add additional print statement and newline to output

--- print_statements.php
+++ print_statements.php
@@ -2,6 +2,7 @@
 
 //Write down a PHP program to display “The pen is “mightier”” than the sword” in your web browser. 
 
-echo "The pen is \"mightier\" than the sword";
+echo "The pen is \"mightier\" than the sword \n"; 
 
 
+echo "Actions speak \"louder\" than words.";
