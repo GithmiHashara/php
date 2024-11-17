c851cad527b12a7c827230dd2d9ff91d58dd8e77
Githmi Hashara <pdghashara@std.appsc.sab.ac.lk>
2024-11-17 19:55:17 +0530

    Add for-each and for loop examples to demonstrate basic looping constructs

---
+++ Loops/for-each_loop.php
@@ -0,0 +1,9 @@
+<?php
+// Define an array
+$fruits = array("Apple", "Banana", "Orange", "Grapes");
+
+// Loop through each item in the array
+foreach ($fruits as $fruit) {
+    echo "Fruit: " . $fruit . "<br>";
+}
+?>
---
+++ Loops/for_loop.php
@@ -0,0 +1,6 @@
+<?php
+// A simple for loop to print numbers from 1 to 5
+for ($i = 1; $i <= 5; $i++) {
+    echo "Number: " . $i . "<br>";
+}
+?>
